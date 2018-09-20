bfe7d491c7944053c948125be31535a7594b9889
Leo <leo.m@hotmail.co.uk>
2018-09-20 14:16:46 +0100

    Show list of people in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import PersonList from "./Components/PersonList";
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <PersonList />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
